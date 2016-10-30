7077bf86a59ddc0db3ace87158946d58b817bc32
Dennis Iversen <dennis.iversen@gmail.com>
2016-10-30 18:02:16 +0100

    fixes

--- setup.sh
+++ setup.sh
@@ -2,5 +2,6 @@
 mkdir -p public 
 cp -n styles.less public/ 
 cp index.html public/ 
+cp -rf node_modules/bootstrap public/
 lessc --source-map=output/styles.map public/styles.less public/styles.css
 
